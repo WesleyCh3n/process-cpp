4e07a7378c0b40c46c828e9b21f80a40c15bd987
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2024-09-19 15:18:14 +0800

    add: pipe 2 process example

---
+++ example/pipe2.cpp
@@ -0,0 +1,46 @@
+#include "../src/process.hpp"
+
+#include <cassert>
+#include <iomanip>
+#include <iostream>
+
+using namespace process;
+int main(int argc, char *argv[]) {
+  if (argc == 2 && std::string(argv[1]) == "out") { // output mode
+    std::vector<std::string> strs{"hello\n", "from\n", "process 1\n"};
+    for (auto &str : strs)
+      std::cout << str;
+    return 0;
+  }
+  if (argc == 2 && std::string(argv[1]) == "in") { // input mode
+    std::string str;
+    for (int i = 0; i < 3; i += 1) {
+      getline(std::cin, str);
+      std::cout << "get: " << std::quoted(str) << '\n';
+    }
+    return 0;
+  }
+
+  try {
+    {
+#ifndef _WIN32
+      std::string app = "sh";
+      std::vector<std::string> args{"-c", std::string(argv[0]), "in"}; // TODO:
+#else
+      std::string app = "cmd";
+      std::vector<std::string> args1{"/c", std::string(argv[0]), "out"};
+      std::vector<std::string> args2{"/c", std::string(argv[0]), "in"};
+#endif
+      auto child1 = Command(app).args(args1).std_out(Stdio::pipe()).spawn();
+      assert(child1.io_stdout);
+      auto child2 = Command(app)
+                        .args(args2)
+                        .std_in(Stdio::from(std::move(*child1.io_stdout)))
+                        .spawn();
+
+      ExitStatus status = child2.wait();
+    }
+  } catch (const std::exception &e) {
+    std::cerr << e.what();
+  }
+}
